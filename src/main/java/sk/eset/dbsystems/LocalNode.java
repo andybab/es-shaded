41eacbf02ac1746828dc2c293161cc524ec93318
Andrej Babolčai <babolcai@eset.sk>
2021-04-29 16:04:02 +0200

    First 'seems to work' ES6 version

---
+++ src/main/java/sk/eset/dbsystems/LocalNode.java
@@ -0,0 +1,21 @@
+package sk.eset.dbsystems;
+
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.node.Node;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.transport.Netty4Plugin;
+import org.elasticsearch.node.InternalSettingsPreparer;
+
+
+class LocalNode extends Node {
+    LocalNode(Settings settings) {
+        super(new Environment(settings, null), Arrays.asList(Netty4Plugin.class), true);
+    }
+
+    @Override
+    protected void registerDerivedNodeNameWithLogger(String nodeName) { }
+}
